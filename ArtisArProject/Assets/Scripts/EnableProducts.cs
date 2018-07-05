0b7263c5f16f8f8f7b2afda3d9beba8ffe7af749
Marq91 <anibal.marquez.91@gmail.com>
2018-07-04 22:04:48 -0400

    Add Base
    Adding Arcore and Empty Object with Test Sofa

---
+++ ArtisArProject/Assets/Scripts/EnableProducts.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using GoogleARCore;
+
+public class EnableProducts : MonoBehaviour {
+
+    //Script Para Habilitar o Deshabilitar un Producto en la Escena
+
+    public GameObject myProduct;
+
+	
+	void Start ()
+    {
+        gameObject.SetActive(true);
+
+
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+
+
+	}
+}
